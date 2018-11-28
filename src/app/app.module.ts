76fc9e452a6f4f5a30da656b3780783f1c8076bb
nasirlodhi <44997612+nasirlodhi@users.noreply.github.com>
2018-11-28 15:27:39 -0800

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
-import { WeatherService } from 'src/app/weather/weather.service'
+import { WeatherService } from './weather/weather.service'
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [WeatherService],
   bootstrap: [AppComponent],
 })
--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { environment } from '../../environments/environment'
+import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
+
+import { environment } from '../../environments/environment'
 import { ICurrentWeather } from '../interfaces'
 
 interface ICurrentWeatherData {
